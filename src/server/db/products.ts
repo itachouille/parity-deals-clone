0b40b2cc41300c567e1fa849e0f85185c0f9f2da
Anthony David <anthony.david34@sfr.fr>
2024-10-24 16:58:52 +0200

    caching systems implementation

--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,8 +1,14 @@
 import { db } from "@/drizzle/db";
 import { ProductCustomizationTable, ProductTable } from "@/drizzle/schema";
+import {
+  CACHE_TAGS,
+  dbCache,
+  getUserTag,
+  revalidateDbCache,
+} from "@/lib/cache";
 import { and, eq } from "drizzle-orm";
 
-export function getProducts(userId: string, { limit }: { limit?: number }) {
+function getProductsInternal(userId: string, { limit }: { limit?: number }) {
   return db.query.ProductTable.findMany({
     where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
     orderBy: ({ createdAt }, { desc }) => desc(createdAt),
@@ -10,11 +16,19 @@ export function getProducts(userId: string, { limit }: { limit?: number }) {
   });
 }
 
+export function getProducts(userId: string, { limit }: { limit?: number }) {
+  const cacheFn = dbCache(getProductsInternal, {
+    tags: [getUserTag(userId, CACHE_TAGS.products)],
+  });
+
+  return cacheFn(userId, { limit });
+}
+
 export async function createProduct(data: typeof ProductTable.$inferInsert) {
   const [newProduct] = await db
     .insert(ProductTable)
     .values(data)
-    .returning({ id: ProductTable.id });
+    .returning({ id: ProductTable.id, userId: ProductTable.clerkUserId });
 
   try {
     await db
@@ -29,6 +43,12 @@ export async function createProduct(data: typeof ProductTable.$inferInsert) {
     await db.delete(ProductTable).where(eq(ProductTable.id, newProduct.id));
   }
 
+  revalidateDbCache({
+    tag: CACHE_TAGS.products,
+    userId: newProduct.userId,
+    id: newProduct.id,
+  });
+
   return newProduct;
 }
 
@@ -42,6 +62,14 @@ export async function deleteProduct({
   const { rowCount } = await db
     .delete(ProductTable)
     .where(and(eq(ProductTable.id, id), eq(ProductTable.clerkUserId, userId)));
+
+  if (rowCount > 0) {
+    revalidateDbCache({
+      tag: CACHE_TAGS.products,
+      userId,
+      id,
+    });
+  }
 
   return rowCount > 0;
 }
